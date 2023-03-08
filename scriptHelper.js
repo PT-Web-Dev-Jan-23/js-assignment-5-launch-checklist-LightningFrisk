ecf3dca3a999e0614a16a66570fa1ee4009c755b
Frisk <bmxcaleb@gmail.com>
2023-03-07 21:24:32 -0600

    index.html, implemented input validation
    undid fucking with index.html, implemented input validation

--- scriptHelper.js
+++ scriptHelper.js
@@ -35,7 +35,19 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     let pilotStatus = document.getElementById("pilotStatus");
     let copilotStatus = document.getElementById("copilotStatus");
 
-    if 
+    let entries = [pilot, copilot, fuelLevel, cargoLevel]
+    let validatedEntries
+    for (i=0; i < entries.length; i++){
+       validatedEntries.push(validateInput(entries[i])); //runs each entry thru validate input and pushes to an array validatedEntries
+    }; 
+    if (validatedEntries[0] === "Is a Number" || validatedEntries[0] === "Empty" || validatedEntries[1] === "Is a Number" || validatedEntries[1] === "Empty")
+    {//checks pilot and copilot number if they're valid, if not throws something for user to do
+        alert("All fields require valid input.");
+    } else {
+        
+        if cargoMass <= 10000
+            list.style.visibility = "visible";
+    }
 }
 
 async function myFetch() {
